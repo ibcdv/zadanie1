5d72744c44782f877dfec0306fd6a8b1c06c4175
Iga Borowska <iga.borowska@gmail.com>
2024-03-13 10:59:28 +0100

    Dodanie prostokata

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,9 @@
 bok_a = 10.5
+bok_b = 10
 
 pole_kw = bok_a * bok_a
+pole_prost = bok_a * bok_b
 
 print("Pole kwadratu wynosi: " + str(pole_kw))
+print("Pole prostokata wynosi: " + str(pole_prost))
 
