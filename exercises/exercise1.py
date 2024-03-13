bc75652efb3b29ac40c11d8ca889c31122151e33
Iga Borowska <iga.borowska@gmail.com>
2024-03-13 11:06:21 +0100

    Dodanie kola

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,9 +1,13 @@
 bok_a = 10.5
 bok_b = 10
+pi = 3.141592653589793
+promien_r = 5
 
 pole_kw = bok_a * bok_a
 pole_prost = bok_a * bok_b
+pole_kola = pi * promien_r * promien_r
 
 print("Pole kwadratu wynosi: " + str(pole_kw))
 print("Pole prostokata wynosi: " + str(pole_prost))
+print("Pole kola wynosi: " + str(pole_kola))
 
